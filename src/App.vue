ed02ee27fb915a7be5b21e3eb1ac59436bea1a03
mpilo.dlamini@yahoo.com <mpilo.dlamini@yahoo.com>
2019-06-22 07:47:43 +0200

    fixing naming standards for vModel removed the v-for error on the ide caused by not setting the v-bind key to the iteration of the for loop. Works perfectly with out it but error is annoying. Did this for best practice

--- src/App.vue
+++ src/App.vue
@@ -53,14 +53,14 @@
                                     type="checkbox"
                                     id="sendmail"
                                     value="SendMail"
-                                    v-model="sendMail"> Send Mail
+                                    v-model="vSendMailModel"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
                                     value="SendInfoMail"
-                                    v-model="sendMail"> Send Infomail
+                                    v-model="vSendMailModel"> Send Infomail
                         </label>
                     </div>
 
@@ -87,10 +87,15 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
                     <label for="priority">Priority</label>
-                    <select
+                    <select 
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                            v-model="vSelectedPriorityModel">
+                        <option 
+                        v-for="priority in vPrioritiesModel"
+                        v-bind:key="priority"
+                        :selected="priority == 'Medium'"
+                        > {{ priority }}</option>
                     </select>
                 </div>
             </div>
@@ -117,10 +122,13 @@
                         <p style="white-space: pre">Message: {{ vMessageModel }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li v-for="mail in sendMail">{{ mail }}</li>
+                            <li 
+                            v-for="mail in vSendMailModel"
+                            v-bind:key="mail"
+                            >{{ mail }}</li>
                         </ul>
                         <p>Gender: {{ gender }}</p>
-                        <p>Priority:</p>
+                        <p>Priority: {{ vSelectedPriorityModel }}</p>
                         <p>Switched:</p>
                     </div>
                 </div>
@@ -139,8 +147,10 @@
                   vAgeModel: 27  
                 },
                 vMessageModel : 'A new Text',
-                sendMail: [],
-                gender: 'Male'
+                vSendMailModel: [],
+                gender: 'Male',
+                vSelectedPriorityModel: 'High',
+                vPrioritiesModel: ['High', 'Medium', 'Low']
                 
             }
         }
